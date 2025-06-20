c83f7cf65f70b1a4f8acb71aa3c04f042ec6f553
Depix David <david.depix@itsf.io>
2025-06-20 16:56:46 +0200

    chore: initialize project

---
+++ src/main/java/fr/depix/bulb_manager/BulbManagerApplication.java
@@ -0,0 +1,13 @@
+package fr.depix.bulb_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BulbManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BulbManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/depix/bulb_manager/BulbManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.depix.bulb_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BulbManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
